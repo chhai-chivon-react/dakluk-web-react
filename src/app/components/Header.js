4d220d2645c016dd084abf618b331585fd2481ff
chhaichivon <chivonchhai@hotmail.com>
2017-11-11 17:32:13 +0700

    clean header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 import { Header, Segment } from 'semantic-ui-react'
 class HeaderApp extends Component {
   render() {
-    return <div>
-    <Header as='h2' attached='top'>
-      Attached Header
-    </Header>
-    <Segment attached>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-      laboris nisi ut aliquip ex ea commodo consequat.
-    </Segment>
-  </div>;
+    return (
+    <div>
+      <Header as='h2' attached='top'>
+        Attached Header
+      </Header>
+      <Segment attached>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.
+      </Segment>
+    </div>);
   }
 }
 export default HeaderApp;
