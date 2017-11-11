6ad69384a0dcb113139f94cade7069280e68963d
chhaichivon <chivonchhai@hotmail.com>
2017-11-11 17:23:20 +0700

    dakluk init

---
+++ src/app/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Header, Segment } from 'semantic-ui-react'
+class FooterApp extends Component {
+  render() {
+    return <Segment clearing>
+    <Header as='h2' floated='right'>
+      Float Right
+    </Header>
+    <Header as='h2' floated='left'>
+      Float Left
+    </Header>
+  </Segment>;
+  }
+}
+export default FooterApp;
